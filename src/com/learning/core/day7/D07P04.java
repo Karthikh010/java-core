b7f6b59115d90a00a398b2f6ceb23ea781211bd7
Karthik H <karthikharikumar010@gmail.com>
2024-03-12 22:22:27 +0530

    D07

---
+++ src/com/learning/core/day7/D07P04.java
@@ -0,0 +1,86 @@
+package com.learning.core.day7;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class D07P04 
+{
+	 public static int evaluateExpression(String expression) 
+	 {
+	        Stack<Integer> numbers = new Stack<>();
+	        Stack<Character> operators = new Stack<>();
+	        
+	        for (int i = 0; i < expression.length(); i++) 
+	        {
+	            char currentChar = expression.charAt(i);
+	            if (Character.isDigit(currentChar)) 
+	            {
+	                StringBuilder operand = new StringBuilder();
+	                while (i < expression.length() && Character.isDigit(expression.charAt(i))) 
+	                {
+	                    operand.append(expression.charAt(i++));
+	                }
+	                i--; // Move back one step as we've advanced extra in the loop
+	                numbers.push(Integer.parseInt(operand.toString()));
+	            } 
+	            else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') 
+	            {
+	                while (!operators.empty() && precedence(operators.peek()) >= precedence(currentChar)) 
+	                {
+	                    int b = numbers.pop();
+	                    int a = numbers.pop();
+	                    char operator = operators.pop();
+	                    numbers.push(applyOperation(a, b, operator));
+	                }
+	                operators.push(currentChar);
+	            }
+	        }
+	        
+	        while (!operators.empty()) 
+	        {
+	            int b = numbers.pop();
+	            int a = numbers.pop();
+	            char operator = operators.pop();
+	            numbers.push(applyOperation(a, b, operator));
+	        }
+	        
+	        return numbers.pop();
+	    }
+	    
+	    public static int precedence(char operator) 
+	    {
+	        switch (operator) 
+	        {
+	            case '+':
+	            case '-':
+	                return 1;
+	            case '*':
+	            case '/':
+	                return 2;
+	        }
+	        return -1;
+	    }
+	    
+	    public static int applyOperation(int a, int b, char operator) 
+	    {
+	        switch (operator) {
+	            case '+':
+	                return a + b;
+	            case '-':
+	                return a - b;
+	            case '*':
+	                return a * b;
+	            case '/':
+	                if (b == 0) throw new UnsupportedOperationException("Cannot divide by zero");
+	                return a / b;
+	        }
+	        return 0;
+	    }
+	    
+	    public static void main(String[] args) 
+	    {
+	        String expression = "10+2*6";
+	        int result = evaluateExpression(expression);
+	        System.out.println(result);
+	    }
+}
